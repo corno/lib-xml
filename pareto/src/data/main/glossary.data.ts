bc38e92b8fee7a0d8fec7a25baef852bced15690
corno <corno.astn.a@schraverus.com>
2023-04-14 22:12:21 +0200

    first commit

---
+++ pareto/src/data/main/glossary.data.ts
@@ -0,0 +1,40 @@
+import * as pd from 'pareto-core-data'
+
+import {
+    aExternalInterfaceReference,
+    aInterface,
+    aInterfaceMethod,
+    aInterfaceReference,
+    array, constructor, data, dictionary, externalTypeReference, glossaryParameter, group, imp,
+    member, nested, procedure, ref, sInterface, sInterfaceMethod, sInterfaceReference, sfunction, streamconsumer, string, taggedUnion, type, typeReference
+} from "lib-pareto-typescript-project/dist/submodules/glossary/shorthands"
+
+import * as g_glossary from "lib-pareto-typescript-project/dist/submodules/glossary"
+
+const d = pd.d
+
+export const $: g_glossary.T.Glossary<pd.SourceLocation> = {
+    'parameters': d({
+    }),
+    'imports': d({
+
+    }),
+    'root': {
+        'namespaces': d({}),
+        'types': d({
+
+        }),
+    },
+    'asynchronous': {
+        'interfaces': d({
+        }),
+        'algorithms': d({
+        }),
+    },
+    'synchronous': {
+        'interfaces': d({
+        }),
+        'algorithms': d({
+        }),
+    },
+}
